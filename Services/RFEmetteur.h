6bc6b47d1246c2684c0b67c1660e1f2110057696
Talkion <moilefo@gmail.com>
2020-10-19 11:05:59 +0200

    Update RFEmetteur.h

--- Services/RFEmetteur.h
+++ Services/RFEmetteur.h
@@ -1,3 +1,9 @@
 #include "stm32f1xx_ll_tim.h"
 
 #define _RF_INPUT_ANGLE_RANGE	10.0
+
+/* RF Input Module can only work with TIM4 */
+void rf_input_start(TIM_TypeDef *RFInputTimer);
+
+/* Returns an angle in [ - _RF_INPUT_ANGLE_RANGE ; _RF_INPUT_ANGLE_RANGE] */
+int rf_input_get_angle(void);

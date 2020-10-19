cf897e69054ba7f13d120515fd842bec5d1aa08c
Talkion <moilefo@gmail.com>
2020-10-19 11:03:22 +0200

    RFEmetteur update

--- Services/RFEmetteur.c
+++ Services/RFEmetteur.c
@@ -2,16 +2,22 @@
 #include "stm32f1xx_ll_bus.h"
 #include "stm32f1xx_ll_gpio.h"
 #include "stm32f1xx_ll_usart.h" 
+#include "stm32f1xx_LL_tim.h"
+#include "MyTimer.h"
 
-/* RF Input works with PB6 and PB7 */
+#ifndef _RF_INPUT_ANGLE_RANGE
+#define _RF_INPUT_ANGLE_RANGE	10.0
+#endif
+
+static TIM_TypeDef *RFInputTimer;
+
+
+/* RF Input works with PB6 or PB7 */
 static void rf_input_init(void) {
 	
-/*channel config required*/
 
-	/* GPIOB6 or GPIOB7 depending on the channel */
-	if	(RFInputChannel == 1) { LLGPIO_struct.Pin = LL8GPIO_PIN_6; }
-	else if (RFInputChannel == 2) { LLGPIO_struct.Pin = LL_GPIO_PIN_7; }
-	
+	/* Input use GPIOB6 */
+	LLGPIO_struct.Pin = LL_GPIO_PIN_6;
 	LLGPIO_struct.Mode = LL_GPIO_MODE_FLOATING;
 	LLGPIO_struct.Pull = LL_GPIO_PULL_DOWN;
 
---
+++ Services/RFEmetteur.h
@@ -0,0 +1,3 @@
+#include "stm32f1xx_ll_tim.h"
+
+#define _RF_INPUT_ANGLE_RANGE	10.0

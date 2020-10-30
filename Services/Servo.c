ead7ce69524632deb67f35c1f8566c9a858f07d4
pierre <pierrecalmettes228@gmail.com>
2020-10-30 12:21:25 +0100

    Ajout config pwm

--- Services/Servo.c
+++ Services/Servo.c
@@ -2,6 +2,8 @@
 #include "stm32f1xx_ll_bus.h"
 #include "stm32f1xx_ll_gpio.h"
 #include "stm32f1xx_ll_tim.h" 
+#include "MyTimer.h"
+
 
 
 
@@ -23,11 +25,48 @@ void servo_pin_conf_io(void) {
 		
 	LL_GPIO_InitTypeDef pin8B ;
 	pin8B.Pin = LL_GPIO_PIN_8 ;
-	pin8B.Mode = LL_GPIO_MODE_OUTPUT;
+	pin8B.Mode = LL_GPIO_MODE_ALTERNATE;
 	pin8B.Speed = LL_GPIO_SPEED_FREQ_LOW;
 	pin8B.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
 	pin8B.Pull = LL_GPIO_PULL_DOWN;
-	LL_GPIO_Init(GPIOA,&pin8B);
+	LL_GPIO_Init(GPIOB,&pin8B);
 	
 	
 }
+
+void change_motor(TIM_TypeDef *TIMER, int pulse){
+    TIMER->CCR1 = TIMER->ARR*pulse/100;
+}
+
+//PWM sur ch3 de tim4
+
+void servo_pwm(void){
+	
+	MyTimer_Conf(TIM4,57599,24);
+	TIM4->CCMR2 &= ~TIM_CCMR2_OC3M_0;
+	TIM4->CCMR2 |= TIM_CCMR2_OC3M_1| TIM_CCMR2_OC3M_2;
+	TIM4->CCER |= TIM_CCER_CC3E;
+	change_motor(TIM4,10); // Réglage de l’impulsion
+
+	MyTimer_Start(TIM4);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Services/Servo.h
@@ -0,0 +1,7 @@
+#include "stm32f103xb.h" 
+
+
+void servo_pin_conf_io(void);
+
+void servo_pwm(void);
+

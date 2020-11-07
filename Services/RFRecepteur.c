097e70e12988f75d7e37919e46384762403f319b
Alexandre Gonzalvez <alexandregonzalvez1@gmail.com>
2020-11-07 15:08:06 +0100

    RFRecepteur : PWM input config

--- Services/RFRecepteur.c
+++ Services/RFRecepteur.c
@@ -16,14 +16,14 @@
 #define RF_INPU_NEUTRAL_DUTY_CYCLE		(((double)RF_INPUT_ARR * 3.0 / 40.0))
 
 static TIM_TypeDef *RFInputTimer;
-static int RFInputChannel;
+//static int RFInputChannel;
 
 static void rf_input_init(void);
 
 void rf_input_start(TIM_TypeDef *_RFInputTimer, int channel)
 {
 	RFInputTimer 		= _RFInputTimer;
-	RFInputChannel = channel;
+	//RFInputChannel = channel;
 	
 	rf_input_init();
 }
@@ -45,12 +45,39 @@ static void rf_input_init(void) {
 	
 
 	/* Input use GPIOB6 or GPIOB7 depending on the channel */
-	if	(RFInputChannel == 1) { LLGPIO_struct.Pin = LL_GPIO_PIN_6; }
+	/*if	(RFInputChannel == 1) { LLGPIO_struct.Pin = LL_GPIO_PIN_6; }
 	else if (RFInputChannel == 2) { LLGPIO_struct.Pin = LL_GPIO_PIN_7; }
+	*/
+	
+	LLGPIO_struct.Pin = LL_GPIO_PIN_6;
 	LLGPIO_struct.Mode = LL_GPIO_MODE_FLOATING;
 	LLGPIO_struct.Pull = LL_GPIO_PULL_DOWN;
-
-}
+	LLGPIO_struct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+	LLGPIO_struct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+	
+	LL_GPIO_Init(GPIOB,&LLGPIO_struct);
+	
+	//config pwm input
+	TIM4->CCMR1 |= TIM_CCMR1_CC1S_0; // put CC1S(0) to 1
+	TIM4->CCMR1 &= ~(TIM_CCMR1_CC1S_1); // put CC1S(1) to 0
+	
+	TIM4->CCER &= ~(TIM_CCER_CC1P);
+	
+	TIM4->CCMR1 &= ~(TIM_CCMR1_CC2S_0); // put CC2S(0) to 1
+	TIM4->CCMR1 |= TIM_CCMR1_CC2S_1; // put CC1S(1) to 0
+	
+	TIM4->CCER |= TIM_CCER_CC2P;
+	
+	TIM4->SMCR |= TIM_SMCR_TS_0 | TIM_SMCR_TS_2;// put TS(0 & 2) to 1
+	TIM4->SMCR &= ~(TIM_SMCR_TS_1);// put TS(1) to 0
+	
+	TIM4->SMCR &= ~(TIM_SMCR_SMS_0) & ~(TIM_SMCR_SMS_1);// put SMS(0 & 1) to 0
+	TIM4->SMCR |= TIM_SMCR_SMS_2;// put SMS(2) to 1
+	
+	// Enable capture
+	TIM4->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E;
+	
+	}
 
 /* Returns an angle in -ange range ; angle range */
 /*
